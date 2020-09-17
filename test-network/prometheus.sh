22034a9e2c7c167cc21c8c38903d65c966c5b6d8
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2020-09-17 16:13:13 +0200

    prometheus update

---
+++ test-network/prometheus.sh
@@ -0,0 +1,1 @@
+docker run --network net_test -p 9090:9090 -v ~/fabric-samples/test-network/prometheus.yml:/etc/prometheus/prometheus.yml -d prom/prometheus
---
+++ test-network/reset.sh
@@ -0,0 +1,4 @@
+rm -r organizations/peerOrganizations
+rm -r organizations/ordererOrganizations
+rm -r organizations/fabric-ca
+rm -r system-genesis-block
